a6eb3fbb05e1c3ff086568730351013cab1de66c
Muslihiddin <32208857+Muslihiddin@users.noreply.github.com>
2022-12-07 18:03:28 +0500

    feat: router

---
+++ src/components/BurgerMenu.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="lg:hidden flex flex-col items-center gap-1 cursor-pointer p-3">
+    <span class="block w-5 h-2px rounded bg-white"></span>
+    <span class="block w-5 h-2px rounded bg-white"></span>
+    <span class="block w-5 h-2px rounded bg-white"></span>
+  </div>
+</template>
